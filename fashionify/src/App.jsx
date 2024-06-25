e0f402651ba07c098121ea9f5a775a98c55a57ee
Sanskar Soni <166742286+Sunscar-Sony@users.noreply.github.com>
2024-06-25 09:36:27 +0530

    Update App.jsx

--- fashionify/src/App.jsx
+++ fashionify/src/App.jsx
@@ -1,11 +1,1 @@
-import './App.css'
-function App() {
-  return (
-    <>
-      <h1>Hello Folks </h1>
-      <p>This is Text Paragraph for Testing, Hello World!</p>
-    </>
-  )
-}
-export default App
 
